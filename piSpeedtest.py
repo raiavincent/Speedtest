79ac3b726ea05b6e45ad775eb3664dbd6e01b1d9
Vincent Raia <vincentraia1@gmail.com>
2021-02-06 15:58:35 -0500

    updated

--- piSpeedtest.py
+++ piSpeedtest.py
@@ -2,6 +2,7 @@ import speedtest
 from datetime import datetime
 import pandas as pd
 
+
 s = speedtest.Speedtest()
 
 megabit = 1000000
@@ -26,7 +27,6 @@ print(currentYear)
 currentHour = now.hour
 print(currentHour)
 
-
 print('Getting your speeds, hot rod.')
 download = s.download()
 upload = s.upload()
@@ -35,10 +35,11 @@ uploadMbps = round(upload/megabit,1)
 print('My download speed is:', downloadMbps,'Mbps.')
 print('My upload speed is:', uploadMbps,'Mbps.')
 
+# speedDict['Now'] = now # considering dropping bc a) it gets truncated and b) its not entirely necessary
 speedDict['Time'] = currentTime
 speedDict['Month'] = currentMonth
 speedDict['Weekday'] = dayOfWeek
-speedDict['Day'] = dayOfMonth # possibly find a better key for this
+speedDict['Day'] = dayOfMonth 
 speedDict['Year'] = currentYear
 speedDict['Hour'] = currentHour
 speedDict['Download'] = downloadMbps
@@ -49,3 +50,5 @@ speedColumns = ['Time','Month','Weekday','Day','Year','Hour','Download','Upload'
 df = pd.DataFrame(columns=speedColumns)
 df = df.append(speedDict,ignore_index=True)
 print(df)
+
+df.to_csv('Test file.csv')
--- piSpeedtestSchedule.py
+++ piSpeedtestSchedule.py
@@ -1,4 +1,6 @@
 import speedtest
+from datetime import datetime
+import pandas as pd
 import schedule
 import time
 
@@ -6,16 +8,47 @@ s = speedtest.Speedtest()
 
 megabit = 1000000
 
-def GetInternetSpeed():
+speedColumns = ['Time','Month','Weekday','Day','Year','Hour','Download','Upload']
+df = pd.DataFrame(columns=speedColumns)
+
+def getSpeeds():
+    speedDict = {}
+
+    today = datetime.today()
+    date = today.strftime("%m/%d/%y")
+    dayOfWeek = today.strftime('%A')
+    now = datetime.now()
+    currentTime = now.strftime("%H:%M:%S")
+    currentMonth = now.strftime('%B')
+    dayOfMonth = datetime.today().day
+    currentYear = now.year
+    currentHour = now.hour
+
     print('Getting your speeds, hot rod.')
     download = s.download()
     upload = s.upload()
     downloadMbps = round(download/megabit,1)
     uploadMbps = round(upload/megabit,1)
-    print('My download speed is:', downloadMbps)
-    print('My upload speed is:', uploadMbps)
+    print('My download speed is:', downloadMbps,'Mbps.')
+    print('My upload speed is:', uploadMbps,'Mbps.')
 
-schedule.every(15).minutes.do(GetInternetSpeed)
+    # speedDict['Now'] = now # considering dropping bc a) it gets truncated and b) its not entirely necessary
+    speedDict['Time'] = currentTime
+    speedDict['Month'] = currentMonth
+    speedDict['Weekday'] = dayOfWeek
+    speedDict['Day'] = dayOfMonth 
+    speedDict['Year'] = currentYear
+    speedDict['Hour'] = currentHour
+    speedDict['Download'] = downloadMbps
+    speedDict['Upload'] = uploadMbps
+
+    global df
+    df = df.append(speedDict,ignore_index=True)
+
+    df.to_csv('Test file.csv')
+    print('Speeds gathered, dataframe updated, saved to csv.')
+
+schedule.every().minute.at(":15").do(getSpeeds)
 
 while True:
     schedule.run_pending()
